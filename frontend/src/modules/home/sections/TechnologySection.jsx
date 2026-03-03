9f1ad01d2b7612998ee9e110095e76ecf12b5004
anshul <anshulgupta2028@gmail.com>
2026-03-03 18:33:56 +0530

    adsfbg

--- frontend/src/modules/home/sections/TechnologySection.jsx
+++ frontend/src/modules/home/sections/TechnologySection.jsx
@@ -162,7 +162,7 @@ export default function TechnologySection() {
         <div className="relative flex justify-center items-center py-4 sm:py-6 md:py-8 lg:py-0 h-[500px] sm:h-[500px] md:h-[600px] lg:h-[700px] [mask-image:linear-gradient(to_right,transparent,black_15%,black_85%,transparent)] [-webkit-mask-image:linear-gradient(to_right,transparent,black_15%,black_85%,transparent)]">
           {/* Central Smartphone */}
           <div className="z-10 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-            <div className="h-[480px] sm:h-[300px] md:h-[400px] lg:h-[500px] xl:h-[600px]">
+            <div className="h-[370px] sm:h-[300px] md:h-[400px] lg:h-[500px] xl:h-[600px]">
               <img
                 src="/imgi_141_N9Y3S3zXP5Dksf1yurkaiZ9KrU.png"
                 alt="HalaPark Technology"
