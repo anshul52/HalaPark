e191f03f61a01073164032b279b742bf81922e80
anshul <anshulgupta2028@gmail.com>
2026-03-13 11:09:39 +0530

    weresfd

--- frontend/src/modules/home/HomePage.jsx
+++ frontend/src/modules/home/HomePage.jsx
@@ -29,7 +29,7 @@ export default function HomePage() {
       <CertificationsSection />
       <ClientsPartners />
       <SmartFeatures />
-      <SayGoodbye />
+      {/* <SayGoodbye /> */}
       <SupportContactCTA />
       {/* <Testimonials /> */}
       {/* <BlogSection /> */}
