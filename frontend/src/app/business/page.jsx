ae9a67b19ca2577fad30f7fca2b954181926af1d
anshul <anshulgupta2028@gmail.com>
2026-03-23 12:37:41 +0530

    afads

--- frontend/src/app/business/page.jsx
+++ frontend/src/app/business/page.jsx
@@ -2,17 +2,20 @@ import BusinessHero from "@/components/business/Hero";
 import BusinessValueProps from "@/components/business/ValueProps";
 import BusinessWhoItsFor from "@/components/business/WhoItsFor";
 import BusinessHowToGetStarted from "@/components/business/HowToGetStarted";
-import BusinessConnectWithUsCTA from "@/components/business/ConnectWithUsCTA";
+import BusinessFeatures from "@/components/business/Features";
+import BusinessWhatWeDo from "@/components/business/WhatWeDo";
+import BusinessCTA from "@/components/business/CTA";
 import HalaParkInAction from "@/modules/home/sections/HalaParkInAction";
-
 export default function BusinessPage() {
   return (
     <div className="min-h-screen bg-white">
       <BusinessHero />
       <BusinessValueProps />
       <BusinessWhoItsFor />
+      {/* <BusinessFeatures /> */}
+      {/* <BusinessWhatWeDo /> */}
       <BusinessHowToGetStarted />
-      <BusinessConnectWithUsCTA />
+      {/* <BusinessCTA /> */}
       <HalaParkInAction />
     </div>
   );
